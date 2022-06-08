b958b17cd411a0f1f5fff6a4a2a86939d655bf1f
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-08 17:28:33 +0200

    66666

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int a;
+	char b;
+	long int d;
+	long long int e;
+	float f;
+printf("the size of an int is: %lu.\n", (unsigned long)sizeof(a));
+printf("the size of a char is: %lu.\n", (unsigned long)sizeof(b));
+printf("the size of a long int is: %lu.\n", (unsigned long)sizeof(d));
+printf("the size of a long long int is: %lu.\n", (unsigned long)sizeof(e));
+printf("the size of a char is: %lu.\n", (unsigned long)sizeof(f));
+return (0);
+}
