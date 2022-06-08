2aec9d946507c0fbf158170200fa9e4349128d37
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-08 17:39:19 +0200

    66666

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long int d;
 	long long int e;
 	float f;
-printf("Size of a char: %lu.\n", (unsigned long)sizeof(b));
-printf("Size of an int: %lu.\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %lu.\n", (unsigned long)sizeof(d));
-printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(e));
-printf("Size of a char: %lu.\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
