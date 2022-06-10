b083f94bf770c66d647f9a719b3c2a6b3612e10c
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-10 14:55:55 +0200

    222tache2222

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-This is the main of my program
+ * Return: Anything that respects the syntax
+**/
+int main(void)
+{
+for (ch = 'lower' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+return (0);
+}
