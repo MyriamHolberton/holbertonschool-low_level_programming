6554a5232d90d4de47bc0bee916563510d6d3258
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-10 15:51:58 +0200

    333tache3333333

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-This is the main of my program
+ * Return: Anything that respects the syntax
+**/
+int main(void)
+{
+char lit;
+for (lit = 'a'; lit <= 'z'; lit++)
+{
+	putchar(lit);
+}
+for (lit = 'A'; lit <= 'Z'; lit++)
+{
+	putchar(lit);
+}
+	putchar(10);
+
+	return (0);
+	}
