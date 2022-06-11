03150100e11e16ea112e4869dcc61ce7e37e9a54
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-11 13:35:30 +0200

    77task77

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-This is the main of my program
+ * Return: Always 0
+**/
+int main(void)
+{
+char lit;
+for (lit = 'z'; lit >= 'a'; lit++)
+{
+	putchar(lit);
+}
+{
+	putchar(10);
+}
+	return (0);
+	}
