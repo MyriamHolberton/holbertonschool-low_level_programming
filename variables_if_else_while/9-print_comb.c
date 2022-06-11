df5ba8c29aa98235dfb7f47ce424ad7ecc3d0dd8
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-11 14:18:53 +0200

    99task99

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-This is the main of my program
+ * Return: Always 0.
+**/
+int main(void)
+{
+int b;
+for (b = '0'; b <= '9'; b++)
+{
+	putchar(b);
+}
+if (b != '9')
+
+{ 
+	putchar(',');
+}
+{
+	putchar(' ');
+}
+{
+	putchar('\n');
+}
+	return (0);
+}
