e6f53cbc13b2ea0474d4314bbffb2923dcd6f038
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-11 13:25:21 +0200

    66task66

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-This is the main of my program
+ * Return: Always 0.
+**/
+int main(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+{
+	putchar(a);
+}
+{
+	putchar('\n');
+}
+	return (0);
+
+}
