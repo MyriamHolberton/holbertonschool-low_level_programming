856a71168c96e9de4ea665d7fac6631fa430c56c
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-11 14:05:16 +0200

    88task88

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-This is the main of my program
+ * Return: Always 0.
+**/
+int main(void)
+{
+int b;
+char lit;
+
+for (b = '0'; b <= '9'; b++)
+{
+	putchar(b);
+}
+for (lit = 'a'; lit <= 'f'; lit++)
+{
+	putchar(lit);
+}
+{
+	putchar('\n');
+}
+	return (0);
+
+}
