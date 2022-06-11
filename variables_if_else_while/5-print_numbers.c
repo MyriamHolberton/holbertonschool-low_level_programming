3a2d93f15a2e59d57f0c03b03f52f5c9e82da104
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-11 12:32:57 +0200

    555task555

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-This is the main of my program
+ * Return: Anything that respects the syntax
+**/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	printf("% d",i);
+}
+
+	return (0);
+
+}
