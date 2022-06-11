2449399b45f7072f7a046f3b8c4849b6f871b345
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-11 12:53:51 +0200

    5tache5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,14 @@
 /**
  * main-This is the main of my program
  * Return: Anything that respects the syntax
+ * Return: Always 0.
 **/
 int main(void)
 {
 int i;
 
-for (i = 0; i < 10; i+)  
-	printf("% d", i);
+for (i = 0; i < 10; i++)  
+	printf("%d", i);
 
 printf("\n");
 
