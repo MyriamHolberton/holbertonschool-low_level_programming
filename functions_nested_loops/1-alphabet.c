716e82ca4d1455179ba57d48330316566ca1fbad
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-13 14:23:43 +0200

    11task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+
+	}
+}
