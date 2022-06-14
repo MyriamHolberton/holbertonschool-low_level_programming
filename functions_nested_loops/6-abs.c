fd0339fe10b7f5609eedc69235cca4816190d7f9
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-14 11:10:42 +0200

    6task666

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - check the code
+ * @n: variable
+ *
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		_putchar('+');
+		return (n);
+	}
+	else
+		return (-n);
+}
