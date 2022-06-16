1162de4a245bf918c817f1d7f66f2ba5bc09c602
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-16 15:58:56 +0200

    2task22

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _mul -it is a function  .
+ * @a, b: input
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int give;
+	give = a*b;
+	return (give);
+}
+
