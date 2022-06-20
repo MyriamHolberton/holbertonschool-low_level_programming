1fe506800728fe7d740be680bde4458ebbf56efb
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-20 16:32:45 +0200

    2task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ * @s: input
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
