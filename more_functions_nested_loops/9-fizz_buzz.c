cc0a81e09dd7111a4bcc88181a4fb264c23f4fe8
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-20 11:37:29 +0200

    999task999

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,20 @@ int main(void)
 {
 	int i;
 
-		for (i = 1; i <= 100; i++)
-		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-				printf("FizzBuzz");
-			else if (i % 3 == 0)
-				printf("Fizz");
-			else if (i % 5 == 0)
-				printf("Buzz");
-			else
-				printf("%d", i);
-		}
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);		
+
+		if (i == 100)
+			continue;
 		printf("\n");
-		return (0);
+	}
+	return (0);
 }
