b766d897641c3b96e8d8680f236547a458f8f82f
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-23 17:02:33 +0200

    task000

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,17 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int n;
 
-	for (i = 0, n = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
 
-			for (i = 0; i < n; n++)
-			{
-				dest[i] = '\0';
-			}
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (n = 0; src[n] != '\0'; n++, i++)
+	{
+		dest[i] = src[n];
+	}
+
 	return (dest);
 }
 
