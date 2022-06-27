3e644011226d68757d5660a07d02fcbfa9199418
Myriam Belkhiri <5019@holbertonschool.com>
2022-06-27 11:12:18 +0200

    task111

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * __memcpy - prints buffer in hexa
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return: Always (Success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
