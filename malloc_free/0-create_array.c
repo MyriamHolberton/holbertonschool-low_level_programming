9381ca323162d564086ee19fd7cc5f5396f10f43
Myriam Belkhiri <5019@holbertonschool.com>
2022-07-01 17:44:56 +0200

    task00

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - main program
+ * @size: the size of the memory to print
+ * @c: input
+ *
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *n;
+
+	if (size == 0)
+		return (NULL);
+
+	n = malloc(size * sizeof(char));
+	if (n == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; size++)
+	{
+		n[i] = c;
+	}
+	return (n);
+}
