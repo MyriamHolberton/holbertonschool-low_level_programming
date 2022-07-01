587063499d92477ec7f693ab8c24756afea72cc8
Myriam Belkhiri <5019@holbertonschool.com>
2022-07-01 05:49:39 +0200

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of argc
+ * @argv: name of app + any other arguments
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
