c91c9c86f11ea1442f04935ba0371a01c68c1c02
Myriam Belkhiri <5019@holbertonschool.com>
2022-07-01 06:42:05 +0200

    task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of argc
+ * @argv: name of app + any other arguments
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+
+	printf("%d\n", c);
+
+	return (0);
+}
+
+
