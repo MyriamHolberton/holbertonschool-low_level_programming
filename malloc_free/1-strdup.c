7173f061e1df95bc419a037d7725ec5ba008226b
Myriam Belkhiri <5019@holbertonschool.com>
2022-07-04 14:57:14 +0200

    task0

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,8 +8,7 @@
  * @size: The size of the array to be initialized.
  * @c: The specific char to initialize the array with.
  *
- * Return: If size == 0 or the function fails - NULL.
- *
+ * Return: If size == 0 or the function fails - NULL.  
  */
 char *create_array(unsigned int size, char c)
 {
@@ -22,9 +21,10 @@ char *create_array(unsigned int size, char c)
 	n = malloc(size * sizeof(char));
 
 	if (n == NULL)
+
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; size++)
 	{
 		n[i] = c;
 	}
