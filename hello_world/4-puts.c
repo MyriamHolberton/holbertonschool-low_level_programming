418a08e3049bd5b46ea950d0195bbf41041bd919
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-09-30 11:57:06 +0200

    4444

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+1 #include <stdio.h>
+  2 
+  3 /**
+  4  * main: main program
+  5  *
+  6  * Return: always 0
+  7  */
+  8 
+  9 int main(void)
+ 10 {
+ 11 puts("Programming is like building a multilingual \n");
+ 12 return (0);
+ 13 }
+ 
