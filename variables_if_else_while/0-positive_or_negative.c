2eb634a8980e7c466269e8e4152861831b420920
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-03 17:26:13 +0200

    000000

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,14 +16,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive\n");
+printf("%d is positive\n", n);
 {
-if else
-printf("is negative\n");
+else if (n < 0)
+printf("%d is negative\n", n);
 {
-if (n == 0)
-printf("is zero\n");
+else if (n == 0)
+printf("%d is zero\n", n);
+}
+}
 }
 }
 return (0);
-}
+
