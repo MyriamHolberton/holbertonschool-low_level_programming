c449e1ceb9833b89981482dbc72f597d7e2d9676
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-03 13:41:40 +0200

    000000

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * more headers goes there
+ *
+ * main - Entry point
+ * betty style doc for function main goes there
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n == 0)
+{
+printf("is zero\n");
+
+if (n > 0)
+{
+printf("is positive\n");
+}
+else
+{
+printf("is negative\n");
+}
+}
+
+return (0);
+}
