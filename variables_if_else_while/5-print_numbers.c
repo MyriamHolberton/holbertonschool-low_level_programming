ba19179913d384e7df951badc87190e91c495f55
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-05 22:43:46 +0200

    5555

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * conditions and variables
+ * Return: return always success
+ */
+int main(void)
+{
+
+	int i;
+
+
+	for (i = 0; i >= 9; i++)
+	{
+
+		printf("%d\n", i);
+	}
+
+		return (0);
+}
