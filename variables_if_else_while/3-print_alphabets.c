d7656fef91673ff6c740d2f74703cde84fe13b53
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-05 20:00:35 +0200

    3333

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -15,7 +15,12 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
-		putchar('\n');
 	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 	return (0);
+
 }
