0759577cdc6ae200dc8af0cc6a3bc2f154bee383
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-05 16:41:26 +0200

    3333

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - fonction principale
+ *
+ * exercice sur les conditions
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
