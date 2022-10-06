1fe781471d186bb7ded1d60114acf5f5308c5301
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-06 07:17:15 +0200

    66666

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * conditions and variables
+ * Return: return always success
+ */
+
+int main(void)
+{
+
+	char ch;
+
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+
+		putchar(ch);
+	}
+	{
+		putchar('\n');
+	}
+
+		return (0);
+}
