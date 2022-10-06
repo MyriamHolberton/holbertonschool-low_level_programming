df1f774b392e2f8da3e8a8ea7da9e5ffae30baa8
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-06 07:25:21 +0200

    7777

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * conditions and variables
+ * Return: return always success
+ */
+
+int main(void)
+{
+
+	int i;
+
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+
+		putchar(i);
+	}
+	{
+		putchar('\n');
+	}
+
+		return (0);
+}
