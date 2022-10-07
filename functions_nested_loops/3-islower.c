c229d0b82cf27df1c1836539d496ddefbf22a8a2
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-07 12:42:34 +0200

    3333

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _islower - main function
+ * @c: input
+ * Return: return always  success
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' &&  c <= 'z')
+
+	return (1);
+
+	else
+
+	return (0);
+
+}
