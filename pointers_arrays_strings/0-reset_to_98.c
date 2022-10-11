c5c62338e9751ea4ca8f03aac866204bb5caec9a
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-11 14:24:20 +0200

    0000

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* reset_to_98 - main function
+* @*n: input
+*
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+
+
+*n = 98;
+}
