94ab15354470d8c5660f190e46c77a77eb80ad9a
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-12 14:23:57 +0200

    6969698582

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse
+* @s: input
+*
+* Return: always 0 (success)
+*/
+void print_rev(char *s)
+{
+int c = 0;
+
+while (s[c] != '\0')
+{
+	c++;
+}
+for (c - = 1; c >= 0; c--)
+{
+	_putchar(s[c]);
+}
+_putchar('\n');
+}
+
