173ca1226f61b52282a64c89ef79a598ecf42d08
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-12 18:31:10 +0200

    5555

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: input
+*
+* Return: always 0 (success)
+*/
+void rev_string(char *s)
+{
+int i = 0;
+int len = 10;
+int tmp = 0;
+int j = 0;
+
+for (i = len-2, j = 0; i >= len / 2; i--, j++)
+{
+	tmp = s[j];
+	s[j] = s[i];
+	s[i] = tmp;
+}
+}
