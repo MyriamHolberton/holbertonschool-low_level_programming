0bdb0c9d92e1c4fb5afa3d23a3ee2908a644a713
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-20 14:17:15 +0200

    0000

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _puts_recursion - Prints a string
+* @s: input
+*
+* Return: always 0 (success)
+*/
+
+void _puts_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+	_putchar('\n');
+}
+
+
+	 _putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
