bf82a65f9e4d78bf84a84175cf080da7479c6695
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-21 09:10:22 +0200

    1111

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: input
+*
+* Return: always 0 (success)
+*/
+
+int _strlen_recursion(char *s)
+{
+
