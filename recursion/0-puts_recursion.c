87ba68d904ac9260d6567bc9331b36ea272a4ba7
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-21 11:48:57 +0200

    0000

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -14,11 +14,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 
-	 _putchar(*s);
+{
+	_putchar(*s);
 	s++;
-
 	_puts_recursion(s);
-
-	_putchar('\n');
 }
+	_putchar('\n');
 }
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
+* @n: input
+* 
+* Return: always 0 (success)
+*/
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
