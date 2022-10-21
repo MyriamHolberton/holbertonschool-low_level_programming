eb9839b1e9b98d1fcc6aa914d41999e0a50901f5
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-21 15:19:31 +0200

    0000

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _sqrt - return the natural square root
+ * @n: input
+ * @i: input
+ *
+ * Return: the natural square root
+ */
+
+/**
+* _sqrt_recursion - that returns the natural square root of a number
+* @n: input
+*
+* Return: always 0 (success)
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+int _sqrt(int n, int i)
+{
+int sqrt = i * i;
+
+if (sqrt > n)
+	return (-1);
+if (sqrt == n)
+	return (i);
+	return (_sqrt(n, i + 1));
+}
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,17 +4,33 @@
 #include "main.h"
 
 /**
-* is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
+* check_prime - check if number is prime 
 * @n: input
-* 
-* Return: always 0 (success)
+* @i: input
+*
+* Return: 1 for prime or composite
+*
 */
 
 int is_prime_number(int n)
 {
-if (n <= 1)
-return (0);
-if (y == 0)
-return (1);
-return (x * _pow_recursion(x, y - 1));
+	return (check_prime(n, 1));
+}
+
+/**
+* is_prime_number - returns 1 if a prime number otherwise return 0
+* @n: input
+*
+* Return: always 0 (success)
+*/
+
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+	return (0);
+	if (n % i == 0 && i > 1)
+	return (0);
+	if ((n / i) < i)
+	return (1);
+	return (check_prime(n, i + 1));
 }
