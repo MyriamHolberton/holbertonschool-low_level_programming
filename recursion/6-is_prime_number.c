29638a9ed4509d0302b6f994cf134945ffacda9e
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-21 16:12:25 +0200

    66666

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,24 +4,12 @@
 #include "main.h"
 
 /**
-* check_prime - check if number is prime 
+* check_prime - check if number is prime
 * @n: input
 * @i: input
 *
 * Return: 1 for prime or composite
 *
-*/
-
-int is_prime_number(int n)
-{
-	return (check_prime(n, 1));
-}
-
-/**
-* is_prime_number - returns 1 if a prime number otherwise return 0
-* @n: input
-*
-* Return: always 0 (success)
 */
 
 int check_prime(int n, int i)
@@ -33,4 +21,16 @@ int check_prime(int n, int i)
 	if ((n / i) < i)
 	return (1);
 	return (check_prime(n, i + 1));
+}
+
+/**
+* is_prime_number - returns 1 if a prime number otherwise return 0
+* @n: input
+*
+* Return: always 0 (success)
+*/
+
+int is_prime_number(int n)
+{
+return (check_prime(n, 1));
 }
