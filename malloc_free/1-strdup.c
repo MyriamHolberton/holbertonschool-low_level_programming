9b33be5abb7c0ae27782a9de5f964999e9d74bc8
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-10-25 18:03:26 +0200

    1111

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -17,11 +17,17 @@ int a = 0, i = 1;
 char *s;
 if (str == NULL)
 return (NULL);
+
 while (str[i])
 {
 	i++;
 }
+
 s = malloc((sizeof(char) * i) + 1);
+
+if (s == NULL)
+return (NULL);
+
 while (a < i)
 {
 s[a] = str[a];
