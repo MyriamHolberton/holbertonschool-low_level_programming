0a6a2e6030f968d38060d610b881ed1c04929af8
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-11-04 13:20:48 +0100

    001001000

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,7 +6,8 @@
 
 /**
 * sum_them_all - returns the sum of all its parameters
-* @n: input
+* @n: the number of parameters passed by the function
+* @...: a variable number of parameters to calculate the sum of
 *
 * Return: if n == 0
 */
@@ -20,8 +21,8 @@ int sum = 0;
 
 if (n != 0)
 {
-va_start(args, n);
-while (i < n)
+	va_start(args, n);
+	while (i < n)
 {
 	sum += va_arg(args, int);
 	i++;
