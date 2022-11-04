cf39b31e0a64906b3744947e2cc230eb3db8c180
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-11-04 14:52:33 +0100

    0000

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -5,12 +5,12 @@
 #include <stdarg.h>
 
 /**
-* sum_them_all - returns the sum of all its parameters
-* @n: the number of parameters passed by the function
-* @...: a variable number of parameters to calculate the sum of
-*
-* Return: if n == 0
-*/
+ * sum_them_all - returns the sum of all its parameters
+ * @n: the number of parameters passed by the function
+ * @...: a variable number of parameters to calculate the sum of
+ *
+ * Return: if n == 0
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -5,13 +5,12 @@
 #include <stdarg.h>
 
 /**
-* print_numbers -prints numbers, followed by a new line
-* @separator: the string to be printed between numbers
-* @n: the number of integers passed to the function
-* @...: a variable number of numbers to be calculate
-*
-* Return:
-*/
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ *
+ * Return: nothing
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
