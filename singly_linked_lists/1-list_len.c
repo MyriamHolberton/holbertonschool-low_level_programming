49285f326c19d7446a49cb7e578205fdd4a0e608
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-11-16 10:19:44 +0100

    1111

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,15 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include "main.h"
+#include "lists.h"
 
 /**
-* print_list -  prints all the elements of a list_t list
-* @h: input
+* list_len - returns the number of elements in a linked list_t list
+* @h: a linked list
 *
-* Return: always 0 
+* Return: the number of elements of a list
 */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-int t, x, y, z; 
+size_t count = 0;
+
+while (h != NULL)
+{
+
+	h = h->next;
+	count++;
+}
+return (count);
+}
