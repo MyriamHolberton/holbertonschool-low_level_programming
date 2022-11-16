b37a4e46e9bc1d9f6b19ec48565e4d43093480e8
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-11-16 10:01:39 +0100

    hhhh

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* print_list -  prints all the elements of a list_t list
+* @h: input
+*
+* Return: always 0 
+*/
+
+size_t print_list(const list_t *h)
+{
+int t, x, y, z; 
