0478f266b5f43df7e759de21d83447a2c87495cf
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-11-18 23:15:12 +0100

    1010

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - inserts a new node at a given position
+* @head: a pointer to the head of the listint_t list
+* @idx: the index of the listint_t list where the new node should be added
+* @n: the integer for the new node to contain
+*
+* Return: if the list is empty - 0
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	int sum = 0;
+
+
+}
