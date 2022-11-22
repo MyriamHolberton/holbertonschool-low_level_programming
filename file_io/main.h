acc7b0ea8c605fbda9441363990c952dc46097a0
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-11-22 10:19:18 +0100

    hhhh

---
+++ file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+
+#endif
